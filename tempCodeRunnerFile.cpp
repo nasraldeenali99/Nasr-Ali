01415690c35b01404c55ade89362c4e9e2979353
Nasr Aldeen <119610299+nasraldeenali99@users.noreply.github.com>
2024-05-09 18:28:57 +0300

    Add files via upload

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,130 @@
+#include <iostream>
+#include <string >
+using namespace std ;
+int main (){
+//DONE BY NASR EL_DEIN ALI 
+    int year;
+    
+    cout << "Enter a year: ";
+    cin >> year;
+    
+    int century = (year - 1) / 100 + 1;
+    
+    cout << "The year " << year << " is in the " << century << "th century." << endl;
+    
+    return 0;
+}
+
+
+
+  
+
+
+
+
+
+
+
+
+// int n1 ,n2 ,result ;
+// cout << "enter two enteger number  :";
+// cin >> n1 >> n2;
+// char symble =  ('+', '-', '*' , '/') ;
+// cout << "enter your option :\n ('+', '-', '*' , '/')" << symble;
+// cin >> symble;
+
+// if (symble == '+')
+// {
+//   result =  n1 + n2 ;
+// }
+//   else if (symble == '-')
+//   {
+//     result =  n1 - n2 ;
+//   }
+//     else if (symble == '/')
+//     {
+//       if (n2 == 0) 
+//       cout << "invalid input ";
+//       else 
+//       result = n1 / n2 ;
+      
+    
+//     }
+//       else if (symble == '*')
+//       {
+//         result =  n1 * n2 ;
+//       }
+//         else {
+//          cout << "invalid input " ; 
+//         }
+//   cout << result << endl;
+//          return 0 ;
+// }
+         
+         
+
+
+// float x , y ,z ;
+// int avarage ;
+// cout << "Enter SCORE 1 :\n" ;
+// cin >> x ;
+// cout << "Enter SCORE 2 :\n" ;
+// cin >> y ;
+// cout << "Enter SCORE 3:\n" ;
+// cin >> z ;
+// avarage = (x + y + z )/3 ;
+// cout << "avarage = "  << avarage;
+// cin >> avarage ;
+// if (avarage > 90 ) {
+//   cout << "avarage is A" ; }
+//   else if (avarage > 80 ){
+//   cout  << "avarage is B" ; }
+//   else if (avarage > 70 ) {
+//   cout << "avarage is C" ; }
+//   else  if (avarage > 60 ){
+//   cout << "avarage is E" ; }
+//   else if (avarage < 60 ) {
+//   cout << "avarage is F" ; }
+//   else 
+//   cout << "envalid input value" ;
+  
+  
+// int x ; 
+// cout << "Enter an enteger number  " ;
+// cin >> x ;
+// if (x > 0)
+// cout << " positive" ;
+// else if (x < 0 )
+// cout << " negative" ;
+// else 
+//  cout << "zero value"
+
+
+
+
+
+
+
+// string name ;
+// int age ; 
+// char grade ;
+// cout << "Enter your name :\n" ;
+// cin >> name ;
+// cout << "Enter your grade : \n" ;
+// cin >> grade ;
+// cout << "Enter your age :\n" ;
+// cin >> grade ;
+
+
+
+
+//write c++ programming to take from user length and width of rectangle and print area
+//Hint:area=length*width#
+// float area , length , width ; 
+// cout << "Enter a length of rectangle:" ;
+// cin >> length ;
+// cout << "Enter a Width of rectangle:" ;
+// cin >> width ;
+// area = length * width ;
+// cout << "AREA = " << area ;
+// cin >> area ;
